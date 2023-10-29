0de98ab1d9976f18da3c2b5460a3d1402dfd9a3d
Loris Kriyonas <loris.kriyonas@gmail.com>
2023-10-29 15:42:25 +0100

    Added a replay function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <limits.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define INPUT_MAX 4
 
 /* Game logic */
 typedef enum Player {
@@ -12,18 +15,20 @@ typedef enum Player {
 } Player;
 
 #define FIELD_SIZE 9
-Player field[FIELD_SIZE];
-typedef size_t (*input_fn)(Player, const Player[FIELD_SIZE]);
 
-bool move_valid(const Player *f, size_t index) {
+typedef Player Field[FIELD_SIZE];
+
+typedef size_t (*input_fn)(Player, const Field);
+
+bool move_valid(const Field f, size_t index) {
   return f[index] == PLAYER_EMPTY;
 }
 
-void field_insert(Player player, size_t field_index) {
+void field_insert(Field field, Player player, size_t field_index) {
   field[field_index] = player;
 }
 
-bool check_win(Player player, const Player *f) {
+bool check_win(Player player, const Field f) {
   // Check rows
   for (int i = 0; i < 3; i++) {
     if (f[i * 3] == player && f[i * 3 + 1] == player &&
@@ -48,26 +53,25 @@ bool check_win(Player player, const Player *f) {
   return false; // No winning combination found
 }
 
-void field_display(const Player *f) {
+void field_display(const Field f) {
   for (size_t i = 0; i < FIELD_SIZE; i++) {
-    if (i % 3 == 0) {
+    if (i != 0 && i % 3 == 0) {
       printf("\n");
     }
-    printf("%c ", f[i] ? f[i] : '1' + (char)i);
+    printf("%c ", f[i] ? f[i] : '1' + (unsigned char)i);
   }
   printf("\n");
 }
 
-bool turn(Player player, input_fn input) {
+bool turn(Field field, Player player, input_fn input) {
   size_t index = input(player, field);
-  field_insert(player, index);
+  field_insert(field, player, index);
   return check_win(player, field);
 }
 
 /* Input */
 
-size_t player_input(Player player, const Player *f) {
-#define INPUT_MAX 4
+size_t player_input(Player player, const Field f) {
   size_t index = 0;
   bool input_valid = false;
   while (!input_valid) {
@@ -75,8 +79,8 @@ size_t player_input(Player player, const Player *f) {
     char choice[INPUT_MAX];
     fgets(choice, INPUT_MAX, stdin);
     const int base = 10;
-    index = strtol(choice, NULL, base) - 1;
-    if ((0 <= index && index < FIELD_SIZE) && move_valid(f, index)) {
+    index = strtoul(choice, NULL, base) - 1;
+    if (index < FIELD_SIZE && move_valid(f, index)) {
       input_valid = true;
     } else {
       printf("Enter Again!\n");
@@ -85,7 +89,7 @@ size_t player_input(Player player, const Player *f) {
   return index;
 }
 
-bool check_draw(const Player *f) {
+bool check_draw(const Field f) {
   for (size_t i = 0; i < FIELD_SIZE; i++) {
     if (move_valid(f, i)) {
       return false;
@@ -100,7 +104,7 @@ Player switch_player(Player current) {
 
 /* Minmax */
 
-int minmax(bool maximize, Player player, Player *f) {
+int minmax(bool maximize, Player player, Field f) {
   if (check_win(player, f)) {
     return maximize ? 2 : -2;
   }
@@ -140,7 +144,7 @@ int minmax(bool maximize, Player player, Player *f) {
   return score;
 }
 
-size_t ai_input(Player player, const Player *current_field) {
+size_t ai_input(Player player, const Field current_field) {
   size_t best_move = 0;
   int score = INT_MIN;
   const size_t moves[FIELD_SIZE] = {4, 0, 2, 6, 8, 1, 3, 5, 7};
@@ -158,28 +162,55 @@ size_t ai_input(Player player, const Player *current_field) {
       }
     }
   }
-  printf("%c: %ld\n", player, best_move + 1);
+  printf("%c: %u\n", player, (uint32_t)best_move + 1);
   return best_move;
 }
 
-/* Game loop */
-int main(void) {
+void game(const input_fn input[2], const Player players[2]) {
+  Field field = {PLAYER_EMPTY};
   Player winning_player = PLAYER_EMPTY;
-  Player players[] = {PLAYER_X, PLAYER_O};
-  input_fn input[] = {player_input, ai_input};
-
   for (size_t i = 0; i < FIELD_SIZE; i++) {
     field_display(field);
     const Player current_player = players[i % 2];
-    if (turn(current_player, input[i % 2])) {
+    if (turn(field, current_player, input[i % 2])) {
       winning_player = current_player;
       break;
     }
   }
   field_display(field);
-  if (winning_player) {
+  if (winning_player != PLAYER_EMPTY) {
     printf("Player %c won!\n", winning_player);
   } else {
     printf("Draw!\n");
+  }
+}
+
+bool replay(void) {
+  bool invalid_input = true;
+  while (invalid_input) {
+    printf("Continue Playing? [Y/N]: ");
+    char buffer[INPUT_MAX];
+    fgets(buffer, INPUT_MAX, stdin);
+    if (buffer[0] == 'N' || buffer[0] == 'n') {
+      return false;
+    }
+    if (buffer[0] == 'Y' || buffer[0] == 'y') {
+      return true;
+    }
+  }
+  return false;
+}
+
+/* Game loop */
+int main(void) {
+  Player players[] = {PLAYER_X, PLAYER_O};
+  input_fn input[] = {player_input, ai_input};
+
+  bool playing = true;
+  while (playing) {
+    game(input, players);
+    if (!replay()) {
+      playing = false;
+    }
   }
 }
